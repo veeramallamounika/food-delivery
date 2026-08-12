31c6c67a0598db1834b0d68ec982abc6f6574dad
veeramallamounika <mounikaveeramalla559@gmail.com>
2026-08-12 19:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,37 +9,37 @@ st.set_page_config(
 
 # Title
 st.title("🍔 Food Delivery App")
-
 st.write("Welcome to our Food Delivery App!")
 
 # Backend API URL
-backend_url = "http://127.0.0.1:8000/foods"
+backend_url = "https://food-delivery-2-pr16.onrender.com/foods"
 
 # Get food data from backend
 try:
     response = requests.get(backend_url)
 
     if response.status_code == 200:
-
         foods = response.json()
 
         st.subheader("🍽️ Available Food")
 
         for food in foods:
-
             st.write(f"### {food['name']}")
             st.write(f"💰 Price: ₹{food['price']}")
 
             st.button(
                 "Add to Cart",
-                key=food["id"]
+                key=f"cart_{food['id']}"
             )
 
             st.divider()
 
     else:
-        st.error("Backend is not responding.")
+        st.error(
+            f"Backend returned an error. Status code: {response.status_code}"
+        )
 
-except requests.exceptions.ConnectionError:
+except requests.exceptions.RequestException as e:
     st.error("❌ Cannot connect to FastAPI backend.")
-    st.write("Please make sure your backend is running.")
+    st.write("Please check whether your backend URL is working.")
+    st.write(e)
